119af9e6d2f64e0d73154d82220a7f9cffc0b3f4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-17 14:25:07 -0400

    Create model for partron

--- library/db/schema.rb
+++ library/db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017181641) do
+ActiveRecord::Schema.define(version: 20171017182036) do
 
   create_table "authors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.integer "author_id"
+    t.string "title"
+    t.integer "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
