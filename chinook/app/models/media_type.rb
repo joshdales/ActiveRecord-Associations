3e556d92ca3af4f209cae2b331eef47605caa010
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-10-17 16:16:52 -0400

    Create associations from playlists to media type through tracks

--- chinook/app/models/media_type.rb
+++ chinook/app/models/media_type.rb
@@ -1,3 +1,4 @@
 class MediaType < ApplicationRecord
   has_many :tracks
+  has_many :playlists, through: :tracks
 end
--- chinook/app/models/playlist.rb
+++ chinook/app/models/playlist.rb
@@ -2,4 +2,5 @@ class Playlist < ApplicationRecord
   has_and_belongs_to_many :tracks
   has_many :genre, through: :tracks
   has_many :albums, through: :tracks
+  has_many :media_types, through: :tracks
 end
